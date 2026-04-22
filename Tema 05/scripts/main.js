edb1acd52988828ae2578c723fda4e75a427c93f
aike <aikesiero@gmail.com>
2026-04-22 18:34:38 +0200

    Tema 4

---
+++ Tema 05/scripts/main.js
@@ -0,0 +1,20 @@
+import EscenaBase from '.escenaBase.js';
+
+// Configuración general del juego
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: '#87CEEB',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+        
+    },
+    scene: [EscenaBase]
+};
+
+// Crear el juego con la configuracion
+const game = new Phaser.Game(config);
